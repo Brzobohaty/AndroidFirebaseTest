727a69ea32dcad6179b8c1f1c0f6f6c51457e3dd
Jan Brzobohatý <jan.brzobohaty@post.cz>
2017-05-19 17:20:13 +0200

    Implemented firebase

--- app/src/main/java/com/crossmasters/myapplication/DisplayMessageActivity.java
+++ app/src/main/java/com/crossmasters/myapplication/DisplayMessageActivity.java
@@ -5,7 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 public class DisplayMessageActivity extends AppCompatActivity {
+    private FirebaseAnalytics mFirebaseAnalytics;
+    private final String pageType = "other";
 
     @Override
     /**https://developer.android.com/training/basics/firstapp/starting-activity.html*/
@@ -20,5 +24,15 @@ public class DisplayMessageActivity extends AppCompatActivity {
         // Capture the layout's TextView and set the string as its text
         TextView textView = (TextView) findViewById(R.id.textView);
         textView.setText(message);
+
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Bundle params = new Bundle();
+        params.putString("screen_name", pageType);
+        mFirebaseAnalytics.logEvent("screenview", params);
     }
 }
--- app/src/main/java/com/crossmasters/myapplication/MainActivity.java
+++ app/src/main/java/com/crossmasters/myapplication/MainActivity.java
@@ -6,18 +6,34 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
-
+    private FirebaseAnalytics mFirebaseAnalytics;
+    private final String pageType = "home";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Bundle params = new Bundle();
+        params.putString("screen_name", pageType);
+        mFirebaseAnalytics.logEvent("screenview", params);
     }
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
+        Bundle params = new Bundle();
+        params.putString("button", "send");
+        mFirebaseAnalytics.logEvent("buttonClick", params);
+
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
